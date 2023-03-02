6646f8c49ad1cc0a9a67257f142cc22aaf2911c1
Sanguinaris <wieichdashasse@gmail.com>
2023-03-02 23:07:15 +0000

    updates and vite stuff

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    minify: 'terser',
+  }
 })
